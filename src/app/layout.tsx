36eb7fbc82f5ddf9514fe899cba8a0cf5c2a6e40
domyeong <wjsehaud9413@naver.com>
2025-01-10 21:18:44 +0900

    feat: pretendard 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import localFont from "next/font/local";
 import LayoutRecoil from "./layout.recoil";
 
 export const metadata = {
   title: "myfair front pre-course",
   description: "todolist",
 };
+
+const pretendard = localFont({
+  src: [
+    {
+      path: '../public/fonts/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
+
 
 export default function RootLayout({
   children,
@@ -13,7 +30,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={pretendard.className}>
         <LayoutRecoil>{children}</LayoutRecoil>
       </body>
     </html>
